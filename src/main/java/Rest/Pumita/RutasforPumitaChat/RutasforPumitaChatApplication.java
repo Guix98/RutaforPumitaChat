2d49f8699ff8df54207f8547289c94dc0e4e2fca
Guido Hernani <guimega.gh@gmail.com>
2019-09-12 15:40:34 -0400

    spring initializr

---
+++ src/main/java/Rest/Pumita/RutasforPumitaChat/RutasforPumitaChatApplication.java
@@ -0,0 +1,13 @@
+package Rest.Pumita.RutasforPumitaChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasforPumitaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasforPumitaChatApplication.class, args);
+	}
+
+}
